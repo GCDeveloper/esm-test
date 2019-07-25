29cfd3d35f143b9446719f68b6d73e6911986de8
George Campbell <campbell.goe@gmail.com>
2019-07-25 12:00:01 +0100

    update webpack etc. for import as npm package

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
+const libraryName = 'esm-test';
 module.exports = {
+  entry: path.join(__dirname, './src/index.js'),
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'Xample.js',
+    library: libraryName,
+    libraryTarget: 'umd',
+    publicPath: '/dist/',
+    umdNamedDefine: true,
+  },
   module: {
     rules: [
       {
@@ -9,5 +19,26 @@ module.exports = {
         },
       },
     ],
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    },
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM',
+    },
   },
 };
